b387dde07cc13b1f564529dd752f1cd00247e109
29303 <2930382766@qq.com>
2026-03-28 11:14:32 +0800

    第一次提交

---
+++ study/script.js
@@ -0,0 +1,10 @@
+﻿function createParagraph() {
+  const para = document.createElement('p');
+  para.textContent = '你点击了按钮，已新增一个段落。';
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+for (const button of buttons) {
+  button.addEventListener('click', createParagraph);
+}
